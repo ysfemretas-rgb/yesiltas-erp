1b7ac656b4e75683e47b63901633db7cfc762f60
ysfemretas-rgb <ysfemretas@icloud.com>
2026-07-26 11:47:55 +0300

    yesiltas-erp-phase1-FIXED

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
-  output: 'standalone'
+  images: { unoptimized: true }
 }
 module.exports = nextConfig
